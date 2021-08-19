ed35c4e18bc3b946dc979b0eb830e2dfcd71664f
leice <leiceleice@mengxiang.com>
2021-08-19 19:19:27 +0800

    第一次代码提交

---
+++ helloworld/src/main/java/com/lxi/test/One.java
@@ -0,0 +1,15 @@
+package com.lxi.test;
+
+import org.omg.CORBA.Object;
+
+/**
+ * @Author: leice
+ * @Description: 类
+ * @DateTime: 2021/8/19 19:14
+ **/
+public class One {
+    public static void main(String[] args) {
+        int i =1;
+        System.out.println(i++);
+    }
+}
